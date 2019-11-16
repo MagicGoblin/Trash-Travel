2d7def2c1376a2cd5b4c4882ccdaa45a09272e06
Gustav <gustav.grube05@gmail.com>
2019-11-16 15:17:07 +0100

    Start
    Start

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D BoatRB;
+    public int Speed;
+    public Vector2 ForwardDirection;
+    public Vector2 BackDirection;
+    public Vector2 RightDirection;
+    public Vector2 LeftDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(BoatRB.position);
+        if (Input.GetKey(KeyCode.W))
+        {
+            BoatRB.AddForce(Speed * ForwardDirection);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            BoatRB.AddForce(Speed * BackDirection);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            BoatRB.AddForce(Speed * RightDirection);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            BoatRB.AddForce(Speed * LeftDirection);
+        }
+    }
+}
